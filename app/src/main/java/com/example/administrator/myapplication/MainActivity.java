c02d1d67f2c17b71121383d087f146bafa1b5781
Richard <hfut20102252@163.com>
2020-09-13 13:24:24 +0800

    first time

--- app/src/main/java/com/example/administrator/myapplication/MainActivity.java
+++ app/src/main/java/com/example/administrator/myapplication/MainActivity.java
@@ -56,30 +56,6 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    public void queryData() {
-
-        StuDBHelper dbHelper = new StuDBHelper(MainActivity.this, "stu_db", null, 1);
-//得到一个可写的数据库
-        SQLiteDatabase db = dbHelper.getReadableDatabase();
-//参数1：表名
-//参数2：要想显示的列
-//参数3：where子句
-//参数4：where子句对应的条件值
-//参数5：分组方式
-//参数6：having条件
-//参数7：排序方式
-        Cursor cursor = db.query("stu_table", new String[]{"id", "sname", "sage", "ssex"}, "id=?", new String[]{"1"}, null, null, null);
-        while (cursor.moveToNext()) {
-            String name = cursor.getString(cursor.getColumnIndex("sname"));
-            String age = cursor.getString(cursor.getColumnIndex("sage"));
-            String sex = cursor.getString(cursor.getColumnIndex("ssex"));
-            System.out.println("query------->" + "姓名：" + name + " " + "年龄：" + age + " " + "性别：" + sex);
-        }
-//关闭数据库
-        db.close();
-    }
-
-
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
